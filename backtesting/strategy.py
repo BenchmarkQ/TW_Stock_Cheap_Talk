9524af2a83425bbc8112218e402d2bcf2c8cb03c
Wei-Zhi <p300053000qin@gmail.com>
2018-06-18 11:41:38 +0800

    naivestrategy

--- backtesting/strategy.py
+++ backtesting/strategy.py
@@ -17,12 +17,32 @@ class NaiveStrategy(Strategy):
         self.year = start_year
         self.month = start_month
     def load_data(self):
-        self.use_data = self.data.get_stock_data(str(self.num), self.year, self.month)
+        try:
+            self.use_data = self.data.get_stock_data(str(self.num), self.year, self.month)
+        except:
+            print('Using Fake Data')
+            self.use_data = pd.read_csv('/home/qin/workspace/TW_Stock_Cheap_Talk/stock_EDA/tw_50.csv', index_col = 0)
+            self.use_data.index = pd.to_datetime(self.use_data.index)
+
     def main(self):
         self.load_data()
         use_data = self.use_data
-        #long timeing
+        date_list = use_data.index 
+        indicator = (use_data['close'] - use_data['open'])/(use_data['open'])
+        pdb.set_trace()
+        indicator = indicator.shift(1).dropna()
+        indicator = pd.DataFrame(indicator, columns = ['sign'])
+        def trans_value(value):
+            if value > 0.012:
+                return 'Short'
+            elif value < -0.012:
+                return 'Long' 
+            elif value < 0.012 and value > -0.012:
+                return 'Hold'
+        indicator['value'] = indicator['sign'].apply(trans_value)
+        return indicator[['value']]
         
 if __name__ == '__main__':
     strategy = NaiveStrategy('0050', 2000, 1)
+    strategy.main()
     pdb.set_trace()
