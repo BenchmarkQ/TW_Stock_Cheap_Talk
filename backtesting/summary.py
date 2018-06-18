b88a17dc44951f3422910d0924b9941767c1fffd
Wei-Zhi <p300053000qin@gmail.com>
2018-06-19 00:27:19 +0800

    update summary

--- backtesting/summary.py
+++ backtesting/summary.py
@@ -2,4 +2,20 @@ import pandas as pd
 import pdb
 
 class TradeResult():
-    pass
+    def __init__(self, trading_detail):
+        self.detail = trading_detail
+        self.asset = trading_detail['asset']
+        self.cal_profit()
+        self.cal_annual_rate
+        self.cal_profit
+        self.cal_sharp_ratio
+    def cal_win_rate(self):
+        pass
+    def cal_profit(self):
+        self.profit = (self.asset[-1] - self.asset[0])/self.asset[0]
+    def cal_sharp_ratio(self):
+        pass
+    def cal_annual_rate(self):
+        pass
+    
+    
