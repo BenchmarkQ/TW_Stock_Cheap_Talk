b90b613da80d40d20d24c9e4b8f143300bfe2157
Wei-Zhi <p300053000qin@gmail.com>
2018-06-19 23:06:39 +0800

    Updating Summary

--- backtesting/summary.py
+++ backtesting/summary.py
@@ -1,21 +1,28 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import pdb
 
 class TradeResult():
     def __init__(self, trading_detail):
+        plt.style.use('seaborn')
         self.detail = trading_detail
         self.asset = trading_detail['asset']
+        self.cal_count()
         self.cal_profit()
-        self.cal_annual_rate
-        self.cal_profit
-        self.cal_sharp_ratio
+        self.cal_profit()
+        self.cal_win_rate()
     def cal_win_rate(self):
-        pass
+        asset_change = self.asset.diff().dropna()
+        self.win_rate = len(asset_change[asset_change > 0])/self.count
+    def cal_max_loss(self):
+        asset_change = self.asset.diff().dropna()
+        self.max_loss = min(asset_change)
     def cal_profit(self):
         self.profit = (self.asset[-1] - self.asset[0])/self.asset[0]
-    def cal_sharp_ratio(self):
-        pass
-    def cal_annual_rate(self):
-        pass
+    def cal_count(self):
+        self.count = len(set(self.asset))
+    def plot(self):
+        self.asset.plot()
+        plt.show()
     
     
